105b6a50063534edee4b063a8e21cddd5763cd37
Shmupo <andrewdoan8b@gmail.com>
2024-11-25 20:51:30 -0800

    Dockerfile and Docker-compose for order-service and mysql containers added.

--- order-consumer/src/main/java/com/Memorium/order_consumer/OrderConsumer.java
+++ order-consumer/src/main/java/com/Memorium/order_consumer/OrderConsumer.java
@@ -8,6 +8,8 @@ import com.Memorium.order_consumer.payload.Order;
 @Slf4j
 @Component
 public class OrderConsumer {
+    // TODO - look into WebClient to send non-blocking HTTP requests to the order-service
+
     @KafkaListener(topics = "order", groupId = "order-consumer-group")
     public void orderConsumer(Order order) {
         log.info("Consumer consume Kafka message -> {}", order);
